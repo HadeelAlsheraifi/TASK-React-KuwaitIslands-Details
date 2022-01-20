1a9ddcafbabf10a980d133c3a8a5fcd338a31bd1
Hadeel <hadeel@safraapp.com>
2022-01-20 12:05:15 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-
-import './App.css';
-import Header from './Components/Header';
-import IslandForm from './Components/IslandForm';
-import IslandList from './Components/IslandList';
-
+import "./App.css";
+import Header from "./Components/Header";
+import IslandForm from "./Components/IslandForm";
+import IslandList from "./Components/IslandList";
+import islands from "./data/islands";
+import { useState } from "react";
 
 function App() {
-
-  let island = {
-    name: "Boubyan",
-    img: "http://photos.wikimapia.org/p/00/02/20/48/92_big.jpg"
-  }
+  const [Island, setIsland] = useState(islands[6]);
 
   return (
-
     <div>
       <Header />
 
       <div className="homePage">
-        <IslandList />
+        <IslandList setIsland={setIsland} />
         <IslandForm island={island} />
+        <island />
       </div>
     </div>
   );
--- src/Components/IslandForm.js
+++ src/Components/IslandForm.js
@@ -1,12 +1,32 @@
+import { useState } from "react";
+
 export default function IslandForm({ island }) {
+  const [visitorname, setVisitorName] = useState("");
+  const [mobile, setMobile] = useState("");
+
+  const bookPopUp = () => {
+    window.confirm(`
+   ${visitorname}
+   ${mobile}
+   ${island.name}`);
+  };
+
+  const changeVisitorName = (e) => {
+    setVisitorName(e.target.value);
+  };
+
+  const changeMobile = (e) => {
+    setMobile(e.target.value);
+  };
+
   return (
     <div className="form">
       <h2>{island.name}</h2>
       <img src={island.img} alt={island.name} />
       <h3>Book a trip to {island.name} island</h3>
-      <input placeholder="Type Full Name" />
-      <input placeholder="Type Phone Number" />
-      <button className="book" onClick={() => {}}>
+      <input onChange={changeVisitorName} placeholder="Type Full Name" />
+      <input onChange={changeMobile} placeholder="Type Phone Number" />
+      <button className="book" onClick={{ bookPopUp }}>
         Book for today!
       </button>
     </div>
